4ad9b17a42635f6d236e98813d5e44f12b34870f
Igo987 <bajrushev@gmail.com>
2023-11-17 18:14:20 +0300

    producer&presenter

--- main.go
+++ main.go
@@ -1,77 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+
+	svc "pr.ru/pkg"
 )
 
-var star = "*"
-
-func getMasks(text, separator string) (new string) { // можно было вторым возвращаемым значением задать error, но не придумал что может пойти не так
-	someFlag := false
-	buffer := make([][]string, 0)
-	word := make([]string, 0)
-	bytes := make([]byte, 0)
-	httpURL := make([]string, 0)
-
-	for _, item := range separator {
-		httpURL = append(httpURL, string(item))
-	}
-	for index, item := range text {
-		if string(item) != " " {
-			word = append(word, (string(text[index])))
-
-		} else {
-			buffer = append(buffer, word)
-			word = []string{}
-		}
-	}
-	buffer = append(buffer, word)
-
-	for ind, w := range buffer {
-		for j := 0; j < len(httpURL); j++ {
-			if (w[j]) == httpURL[j] {
-				someFlag = true
-			} else {
-				someFlag = false
-				if !(someFlag) {
-					break
-				}
-			}
-		}
-		if len(w) <= len(httpURL)+4 { // да, число 4 магическое: (если после httpURL(http://) меньше 5 символов,то, ну наверное, это не URL адрес и можно это слово не проверять)
-			for _, oneWord := range w {
-				word := []byte(oneWord)
-				bytes = append(bytes, word...)
-			}
-
-		} else if someFlag {
-			
-			for indexLetter := range w {
-
-				if someFlag {
-					if indexLetter < len(httpURL) {
-						bytes = append(bytes, []byte(httpURL[indexLetter])...)
-					} else {
-						bytes = append(bytes, []byte(star)...)
-					}
-				}
-			}
-		} else {
-			for _, oneWord := range w {
-				word := []byte(oneWord)
-				bytes = append(bytes, word...)
-			}
-		}
-		if ind < (len(buffer) - 1) {
-			space := []byte(" ")
-			bytes = append(bytes, space...)
-		}
-	}
-	new = string(bytes)
-	fmt.Println(new)
-	return string(new)
-}
 
 func main() {
-	getMasks("Here's my spammy page: http://heher see you.", "http://")
+	path_read := flag.String("path_read", "./src/links.txt", "path to write file")
+	path_write:=flag.String("path_write", "./src/links.txt", "path to read file")
+	flag.Parse()
+	Service := svc.Run(*path_read,*path_write)
+	fmt.Println(Service)
+
 }
--- some_test.go
+++ some_test.go
@@ -4,13 +4,15 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	svc "pr.ru/pkg"
 )
 
 func TestGetMasks(t *testing.T) {
 	text := "http://hehee see"
 	separator := "http://"
 	expected := "http://***** see"
-	actual := getMasks(text, separator)
+	// actual := svc.getMasks(text, separator)
+	actual := svc.GetMasks(text,separator)
 
 	assert.Equal(t, expected, actual)
 }
